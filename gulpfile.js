646f1937f62434630b3acfefb3b55b87cace0158
Jalal Albasri <jalal.albasri@gmail.com>
2018-06-21 03:31:32 +0300

    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,184 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var watch = require('gulp-watch');
+var plugins = require('gulp-load-plugins')();
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var stripDebug = require('gulp-strip-debug');
+var mainBowerFiles = require('main-bower-files');
+var filter = require('gulp-filter');
+var order = require('gulp-order');
+var cleanCss = require('gulp-clean-css');
+var concatCss = require('gulp-concat-css');
+var pug = require('gulp-pug');
+var templateCache = require('gulp-angular-templatecache');
+var sequence = require('gulp-sequence');
+var plumber = require('gulp-plumber');
+var less = require('gulp-less');
+var multiProcess = require('gulp-multi-process');
+var babel = require('gulp-babel');
+
+gulp.task('default', function() {
+  return gutil.log('Gulp is running!');
+});
+
+gulp.task('default', ['watch']);
+
+gulp.task('watch', function() {
+  watch('public/stylesheets/less/*.less', function() {
+    multiProcess(['build-less'], function() {});
+  });
+
+  watch('views/templates/*.pug', function() {
+    gulp.start('build-pug-templatecache');
+  });
+
+  watch('public/javascripts/src/*.js', function() {
+    gulp.start('build-src');
+  });
+});
+
+gulp.task('build-all', function(callback) {
+  sequence('build-scrolls', 'build-spells')(callback);
+});
+
+/**
+ * SCROLLS
+ */
+
+gulp.task('build-scrolls', function(callback) {
+  sequence('build-less', 'build-css')(callback);
+});
+
+gulp.task('build-less', function() {
+  return gulp
+    .src('public/stylesheets/less/style.less')
+    .pipe(plumber())
+    .pipe(less())
+    .on('error', function(err) {
+      gutil.log(err);
+      this.emit('end');
+    })
+    .pipe(
+      plugins.autoprefixer({
+        browsers: [
+          '> 1%',
+          'last 2 versions',
+          'firefox >= 4',
+          'safari 7',
+          'safari 8',
+          'IE 8',
+          'IE 9',
+          'IE 10',
+          'IE 11'
+        ],
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest('public/stylesheets/css'))
+    .on('error', gutil.log);
+});
+
+gulp.task('build-css', function() {
+  var cssFiles = ['public/stylesheets/css/*.css'];
+
+  var ignoreBowerComponents = [];
+
+  function mainBowerFilesFilter(filePath) {
+    var i;
+    for (i = 0; i < ignoreBowerComponents.length; i++) {
+      if (filePath.indexOf(ignoreBowerComponents[i]) !== -1) return false;
+    }
+    return true;
+  }
+
+  return gulp
+    .src(
+      mainBowerFiles({
+        filter: mainBowerFilesFilter
+      }).concat(cssFiles)
+    )
+    .pipe(filter('**/*.css'))
+    .pipe(order(['public/stylesheets/css/style.css']))
+    .pipe(concatCss('scrolls.min.css'))
+    .pipe(cleanCss())
+    .pipe(gulp.dest('public/stylesheets/dist'))
+    .on('error', gutil.log);
+});
+
+/**
+ * SPELLS
+ */
+
+gulp.task('build-spells', function(callback) {
+  sequence('build-pug-templatecache', 'build-src', 'build-js')(callback);
+});
+
+gulp.task('build-pug-templatecache', function(callback) {
+  sequence('build-pug', 'build-templatecache')(callback);
+});
+
+gulp.task('build-pug', function() {
+  var templateFiles = ['views/templates/*.pug'];
+  return gulp
+    .src(templateFiles)
+    .pipe(pug())
+    .pipe(gulp.dest('views/html/'))
+    .on('error', gutil.log);
+});
+
+gulp.task('build-templatecache', function() {
+  return gulp
+    .src('views/html/*.html')
+    .pipe(
+      templateCache('templates.js', {
+        standalone: true,
+        module: 'rpTemplates'
+      })
+    )
+    .pipe(gulp.dest('public/javascripts/src'))
+    .on('error', gutil.log);
+});
+
+gulp.task('build-src', function() {
+  return gulp
+    .src('public/javascripts/src/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('public/javascripts/build/'))
+    .on('error', gutil.log);
+});
+
+gulp.task('build-js', function() {
+  var jsFiles = [
+    'public/javascripts/build/*.js',
+    'public/javascripts/resources/*.js'
+  ];
+  var ignoreBowerComponents = [];
+
+  // http://stackoverflow.com/questions/34547873/exclude-a-folder-from-main-bower-files?lq=1
+  function mainBowerFilesFilter(filePath) {
+    var i;
+    for (i = 0; i < ignoreBowerComponents.length; i++) {
+      if (filePath.indexOf(ignoreBowerComponents[i]) !== -1) return false;
+    }
+    return true;
+  }
+
+  return gulp
+    .src(
+      mainBowerFiles({
+        filter: mainBowerFilesFilter
+      }).concat(jsFiles)
+    )
+    .pipe(filter('**/*.js'))
+    .pipe(stripDebug())
+    .pipe(concat('spells.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/javascripts/dist/'))
+    .on('error', gutil.log);
+});
+
+function onError(err) {
+  console.log(err);
+  this.emit('end');
+}
